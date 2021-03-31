a1d50c3c78a4c295a8a54528053d56cceae886a7
HagathaRS <hagatharosasantosn09@gmail.com>
2021-03-30 22:11:01 -0300

    Criação da rota layout

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', function(req,resp){
 {
     res.send ('esta mensagem é automática!')
 })
+app.get('/layout',function(req,res)
+{
+    res.send ('Este é o novo layout!')
+})
+
 app.listen(3000,function(){
  console.log('conexão inicializada')
 })
