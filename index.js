05b0a0a614c306edfc266fa1c7cf3d041b982b8e
HagathaRS <hagatharosasantosn09@gmail.com>
2021-03-30 21:46:36 -0300

    adição da rota mensagem

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express()
 app.get('/', function(req,resp){
     resp.send('Olá Hágatha!')
 })
-
-
+ app.get('/mensagem',function(req,res)
+{
+    res.send ('esta mensagem é automática!')
+})
 app.listen(3000,function(){
  console.log('conexão inicializada')
 })
