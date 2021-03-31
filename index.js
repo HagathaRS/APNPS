fc3d2a11722fe1514516744a095ef93469f83634
HagathaRS <hagatharosasantosn09@gmail.com>
2021-03-30 22:06:00 -0300

    Adicionando a rota sobre

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', function(req,resp){
 {
     res.send ('esta mensagem é automática!')
 })
+app.get('/sobre',function(req,res)
+{
+    res.send ('Esta página esta sendo ddesenvolvida por Hágatha!')
+})
+ 
 app.listen(3000,function(){
  console.log('conexão inicializada')
 })
