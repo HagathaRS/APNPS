027f66d96551e6eee13ddb5fd0a7bedc65a2f304
HágathaRS <hagatharosasantosn09@gmail.com>
2021-04-03 22:47:35 -0300

    configurações iniciais do projeto

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const express = require ('express')
 const app = express()
+var bodyParser = require ('body-parser')
+var cookieParser = require ('cookie-parser')
+var path = require('path')
+
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set('view engine ','ejs')
+
+app.use(express.static(path.join(__dirname,'public')))
+
+
+
 
 app.get('/', function(req,resp){
     resp.send('Olá Hágatha!')
-})
- app.get('/mensagem',function(req,res)
-{
-    res.send ('esta mensagem é automática!')
-})
-
-app.get('/sobre',function(req,res)
-{
-    res.send ('Esta página esta sendo ddesenvolvida por Hágatha!')
-})
- 
-app.get('/layout',function(req,res)
-{
-    res.send ('Este é o novo layout!')
 })
 
 app.listen(3000,function(){
